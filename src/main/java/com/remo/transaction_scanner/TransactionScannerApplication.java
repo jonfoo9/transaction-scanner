afd199dd37cfd68374a7dbf3a8b758391b4c9fde
Jon Foo <jonfoo.mail@gmail.com>
2025-04-24 16:13:16 +0100

    init

---
+++ src/main/java/com/remo/transaction_scanner/TransactionScannerApplication.java
@@ -0,0 +1,13 @@
+package com.remo.transaction_scanner;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TransactionScannerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TransactionScannerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/remo/transaction_scanner/TransactionScannerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.remo.transaction_scanner;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TransactionScannerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
